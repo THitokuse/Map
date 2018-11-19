64f97069c398d65fae04fff4d964213d5e6e081f
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2018-11-19 16:42:55 +0900

    first commit

---
+++ Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by 長谷川太一朗 on 2018/11/19.
+//  Copyright © 2018年 hasegawa taichiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
